c85bc2e7c47c8d353bd26d35cb0a24925004c8ec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Dashboard_EntityFramework/Form1.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.XtraEditors;
+using DevExpress.DataAccess.EntityFramework;
+
+namespace Dashboard_EntityFramework {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+            InitializeDashboard();          
+        }
+
+        public void InitializeDashboard() {
+            Dashboard dashboard = new Dashboard();
+            DashboardEFDataSource efDataSource = new DashboardEFDataSource();
+            efDataSource.ConnectionParameters =
+                new EFConnectionParameters(typeof(OrdersContext));
+            dashboard.DataSources.Add(efDataSource);
+
+            PivotDashboardItem pivot = new PivotDashboardItem();
+            pivot.DataMember = "Orders";
+            pivot.DataSource = dashboard.DataSources[0];
+            pivot.Rows.AddRange(new Dimension("ShipCountry"), new Dimension("ShipCity"));
+            pivot.Columns.Add(new Dimension("OrderDate"));
+            pivot.Values.Add(new Measure("Freight"));
+
+            ChartDashboardItem chart = new ChartDashboardItem();
+            chart.DataSource = dashboard.DataSources[0];
+            chart.DataMember = "Orders";
+            chart.Arguments.Add(new Dimension("OrderDate", DateTimeGroupInterval.Year));
+            chart.Panes.Add(new ChartPane());
+            SimpleSeries freightSeries = new SimpleSeries(SimpleSeriesType.Bar);
+            freightSeries.Value = new Measure("Freight");
+            chart.Panes[0].Series.Add(freightSeries);
+
+            dashboard.Items.AddRange(pivot, chart);
+            dashboardViewer1.Dashboard = dashboard;
+        }
+    }
+}
---
+++ CS/Dashboard_EntityFramework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using DevExpress.Skins;
+
+namespace Dashboard_EntityFramework {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            SkinManager.EnableFormSkins();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
